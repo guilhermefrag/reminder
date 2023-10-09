190fa04ab84f2bb9111cf9a4332ef38d0a2b83a2
Guilherme Fragnani Bez Fontana <guilhermefragnani@icloud.com>
2023-10-09 17:22:43 -0300

    add stuffs

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -6,8 +6,8 @@ export default function NotFound(statusCode: number) {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
             <p className="text-2xl mb-4">Sorry, the page you are looking for does not exist.</p>
-            <div className="w-48 h-48">
-                <img src={imageUrl} alt={`HTTP ${statusCode}`} className="w-full h-full" />
+            <div className="w-70vw max-w-screen-lg">
+                <img src={imageUrl} alt={`HTTP ${statusCode}`} className="w-full h-auto" />
             </div>
         </div>
     );
